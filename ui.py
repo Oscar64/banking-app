4105e92384ccae98ecf72c749a48b51993f2821e
Oscar Umberson <oscarumberson@gmail.com>
2023-04-26 23:44:18 -0500

    minor main.py and ui.py changes

--- ui.py
+++ ui.py
@@ -1,11 +1,25 @@
 
 
 
+from bank import Bank as b
+from os import system
+import getkeys
+
+
 class UI:
 
     @staticmethod
     def login_menu():
-        pass
+        while True:
+            system('clear')
+            print("""
+1) Log In
+2) Create Account
+3) Exit
+""")
+            
+            
+
 
 
     @staticmethod
@@ -28,3 +42,26 @@ class UI:
         pass
 
 
+"""
+# sample interaction with banking app:
+new_id = create_new_account('misha', 12345)
+print('created account number:', new_id)
+print('remember this number to access it in the future')
+print()
+
+# balance check with correct pin
+balance = check_balance(new_id, 12345)
+print(f'(correct pin) balance for account #{new_id} is {balance}')
+
+# balance cehck with incorrect pin
+balance = check_balance(new_id, 5678)
+print(f'(wrong pin) balance for account #{new_id} is {balance}')
+
+"""
+
+"""
+┌┬─┐ ╔╦═╗ ╒╤═╕ ╓╥─╖
+││ │ ║║ ║ ││ │ ║║ ║
+├┼─┤ ╠╬═╣ ╞╪═╡ ╟╫─╢
+└┴─┘ ╚╩═╝ ╘╧═╛ ╙╨─╜
+"""
