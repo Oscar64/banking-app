a0b1b8451cb5294613d69ff9341d10d6fd3289fe
Oscar Umberson <oscarumberson@gmail.com>
2023-04-27 23:10:22 -0500

    Created prototype of login menu

--- ui.py
+++ ui.py
@@ -3,21 +3,65 @@
 
 from bank import Bank as b
 from os import system
-import getkeys
+from box_string import box_message as box, box_messages as box_list
 
 
 class UI:
 
     @staticmethod
     def login_menu():
+        """Menu for logging into an account; redirects to account_menu and create_account_menu."""
+
         while True:
             system('clear')
-            print("""
+            print(f"""
+{box("Welcome to the bank.")}
 1) Log In
 2) Create Account
 3) Exit
 """)
-            
+        
+            i = input()
+            if i == '1':
+                account_num_input = ''
+                pin_input = ''
+
+                while True:
+                    system('clear')
+                    # Print account_num and pin
+                    print(box_list([f"Account Number: {account_num_input}", f"PIN: {pin_input}"], 30))
+                    
+                    # account_num_input
+                    if len(account_num_input) == 0:
+                        print(box("Please enter your account number."))
+                        temp_input = input()
+                        if len(temp_input) > 0:
+                            account_num_input = temp_input
+                            continue
+
+                    # pin_input
+                    if len(pin_input) == 0:
+                        print(box("Please enter your PIN."))
+                        temp_input = input()
+                        if len(temp_input) > 0:
+                            pin_input = temp_input
+                            continue
+                    
+                    # Attempt to log in
+                    if b.test_login(account_num_input, pin_input):
+                        UI.account_menu(account_num_input)
+                    else:
+                        system('clear')
+                        print(box("Your account number or PIN is wrong. Press [Enter] to try again."))
+                        input()
+
+
+            elif i == '2':
+                UI.create_account_menu()
+
+            elif i == '3':
+                b.end_database_connection()
+                return
             
 
 
@@ -28,18 +72,19 @@ class UI:
 
 
     @staticmethod
-    def account_menu():
+    def account_menu(account_num):
         pass
 
 
     @staticmethod
-    def withdraw_menu():
+    def withdraw_menu(account_num):
         pass
 
 
     @staticmethod
-    def deposit_menu():
+    def deposit_menu(account_num):
         pass
+
 
 
 """
@@ -64,4 +109,23 @@ print(f'(wrong pin) balance for account #{new_id} is {balance}')
 ││ │ ║║ ║ ││ │ ║║ ║
 ├┼─┤ ╠╬═╣ ╞╪═╡ ╟╫─╢
 └┴─┘ ╚╩═╝ ╘╧═╛ ╙╨─╜
+
+
+
+╒════════════╕
+│            │
+╞════════════╡
+│            │
+│            │
+│            │
+╘════════════╛
+
+╒════════════════════════════════╕
+│ Account Number:                │
+│ PIN:                           │
+╞════════════════════════════════╡
+│                                │
+│                                │
+╘════════════════════════════════╛
+
 """
